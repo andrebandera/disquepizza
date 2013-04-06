71cf58bc5f8e1cbf332c93dd84d2b02d0eefad63
avmdb <avmdb@avmdb-note>
2013-04-06 11:52:02 -0400

    Criacao classe

---
+++ disquepizza/src/MainEGit.java
@@ -0,0 +1,14 @@
+
+public class MainEGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Teste EGit - Github !");
+
+	}
+
+}
