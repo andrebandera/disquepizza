4b5b6d17f02fc01a29a60504e83b36c25a188df8
avmdb <avmdb@avmdb-note>
2013-04-06 12:21:23 -0400

    acerto acentuacao

--- disquepizza/src/MainEGit.java
+++ disquepizza/src/MainEGit.java
@@ -6,7 +6,7 @@ public class MainEGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Teste EGit - Github !!! VÃ¡rios teste !");
+		System.out.println("Teste EGit - Github !!! Vários teste !");
 
 	}
 
